5cf0eb1ea2ca3994afd811f88087b981ff04e600
Jacob Madsen <jmadsen24@Jacobs-MacBook-Pro.local>
2019-12-28 11:59:04 -0500

    basic cube working

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import logo from '../../logo.svg';
+// import logo from '../../logo.svg';
 
 class Home extends Component {
 
@@ -8,8 +8,32 @@ class Home extends Component {
 
     return (
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+        {/* <img src={logo} className="App-logo" alt="logo"/> */}
         <Container>
+          <Row>
+            <Col>
+              <h1>
+                Jacob Madsen
+              </h1>
+            </Col>
+          </Row>
+          <br/>
+          <br/>
+          <br/>
+          <Row>
+            <Col>
+            <div id="wrapD3Cube">
+              <div id="D3Cube">
+                <div id="side1"></div>
+                <div id="side3"><h5 style={{ position: 'absolute', top: '25%' }}>Software Developer</h5></div>
+                <div id="side2"><h5 style={{ position: 'absolute', top: '25%' }}>Technology Consultant</h5></div>
+                <div id="side4"><h4 style={{ position: 'absolute', top: '25%' }}>Team Lead</h4></div>
+                <div id="side5"><h4 style={{ position: 'absolute', top: '25%' }}>Data Scientist</h4></div>
+                <div id="side6"></div>
+              </div>
+            </div>
+            </Col>
+          </Row>
           <Row>
             <Col>
               <p>
