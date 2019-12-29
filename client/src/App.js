9fe43cae504b2375046b6aa6c66d9e5d89dcdf98
Jacob Madsen <jmadsen24@Jacobs-MacBook-Pro.local>
2019-12-29 18:26:00 -0500

    fixed navbar highlight bug by using react hooks to update state

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 import { Route, Switch, HashRouter } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -12,19 +12,21 @@ import Contact from './Components/Contact/index.js';
 
 function App() {
 
+  const [active, setActive] = useState('home');
+
   return (
     <div className="App">
       <HashRouter>
         <Navbar bg="light" expand="lg">
-          <LinkContainer exact to='/'><Navbar.Brand>Jacob Madsen</Navbar.Brand></LinkContainer>
+          <LinkContainer exact to='/'><Navbar.Brand onClick={ () => setActive('home') }>Jacob Madsen</Navbar.Brand></LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <LinkContainer to='/about'><Nav.Link>About</Nav.Link></LinkContainer>
-              <LinkContainer to='/skills'><Nav.Link>Skills</Nav.Link></LinkContainer>
-              <LinkContainer to='/experience'><Nav.Link>Work Experience</Nav.Link></LinkContainer>
-              <LinkContainer to='/education'><Nav.Link>Education</Nav.Link></LinkContainer>
-              <LinkContainer to='/contact'><Nav.Link>Contact Me</Nav.Link></LinkContainer>
+              <LinkContainer to='/about'><Nav.Link onClick={ () => setActive('about') } active={ active === 'about' }>About</Nav.Link></LinkContainer>
+              <LinkContainer to='/skills'><Nav.Link onClick={ () => setActive('skills') } active={ active === 'skills' }>Skills</Nav.Link></LinkContainer>
+              <LinkContainer to='/experience'><Nav.Link onClick={ () => setActive('experience') } active={ active === 'experience' }>Work Experience</Nav.Link></LinkContainer>
+              <LinkContainer to='/education'><Nav.Link onClick={ () => setActive('education') } active={ active === 'education' }>Education</Nav.Link></LinkContainer>
+              <LinkContainer to='/contact'><Nav.Link onClick={ () => setActive('contact') } active={ active === 'contact' }>Contact Me</Nav.Link></LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
